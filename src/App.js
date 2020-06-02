6305fed3e4598af3f1f7862e8d609fb16b30857e
Sahal Asghar <sahaldos1@gmail.com>
2020-06-02 16:01:19 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ContactCard from './ContactCard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <>
+      <ContactCard name="John" mobile="917-987-7890" work="800-888-1234" email="john@work.com"/>
+      <ContactCard name="Mike" mobile="347-123-4512" work="800-888-1234" email="mike@work.com"/>
+      <ContactCard name="Sam" mobile="646-319-0009" work="800-888-1234" email="Sam@work.com"/>
+      <ContactCard name="Jake" mobile="646-419-8765" work="800-888-1234" email="jake@work.com"/>
+     </>
+    )
+  }
 }
+
+
 
 export default App;
